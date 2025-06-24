9fa5af780680f48806c94a39dba52762e53cf112
Jatin <jatinverma2428@gmail.com>
2025-06-24 15:43:55 +0530

    push

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
 const section = document.querySelector("section");
 
 
-async function fetchdatafromurl(url){
+async function fetchdatafromurl(url) {
     const response = await fetch(url);
     const result = await response.json();
     console.log(result);
     showdata(result.products);
-    
+
 }
 
 fetchdatafromurl("https://fakestoreapi.in/api/products");
-function showdata(arr){
-    arr.map((product)=>{
+function showdata(arr) {
+    arr.map((product) => {
         const parent = document.createElement("div");
         parent.classList.add("product");
 
+        const anchor = document.createElement("a");
+        anchor.href = "second.html?id=" + product.id;
+
+
+
+
         const img = document.createElement("img");
         img.src = product.image;
+        anchor.append(img);
 
 
         const title = document.createElement("h3");
@@ -35,7 +42,7 @@ function showdata(arr){
         cartBtn.textContent = "Add to Cart";
 
         btnGroup.append(wishlistBtn, cartBtn);
-        parent.append(img, title, price, btnGroup);
+        parent.append(anchor, title, price, btnGroup);
         section.append(parent);
 
 
